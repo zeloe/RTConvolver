a5087017d1379c85eb2108457239f82cea4f2cbf
Zeno Lösch <54742442+zeloe@users.noreply.github.com>
2024-12-09 14:36:37 +0100

    added size menu

--- vst_sources/source/CPU/GUI/GUIDefines.h
+++ vst_sources/source/CPU/GUI/GUIDefines.h
@@ -4,10 +4,14 @@
 #include <JuceHeader.h>
 
 
-const int guiwidth = 400;
-const int guiheight = 400;
-const int offsetx = 80;
-const int offsety = 80;
-const int size = 240;
+const int guiwidth = 250;
+const int guiheight = 250; 
+const juce::Rectangle<int> volumeRect{ 45,60,160,160 };
+const juce::Rectangle<int> sizeRect{ 75,10,100,20 };
 
 const juce::Colour outLineColor = juce::Colours::lightgrey.darker().darker();
+
+static juce::StringArray sizesLabel = { " 0.5 "," 1 ", " 2 " , " 3 " , " 4 " };
+
+
+
